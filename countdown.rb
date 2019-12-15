0900b9390d08adb2952b3e33535533c4791887b7
Karen Daniela <kdaniimora@gmail.com>
2019-12-15 19:08:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
-def countdown (int)
+def countdown(int)
   while int > 0 do
-    puts "#{int} Second (S)!"
+    puts "#{int} Second(S)!"
     int -= 1 
   end
-  
- "Happy New Year"
+  "Happy New Year"
 end
